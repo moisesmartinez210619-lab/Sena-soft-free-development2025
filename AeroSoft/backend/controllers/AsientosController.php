9debec7080f2f11aa92daec87fd0e9f9d5879b0a
trabajossenacristiana-cmd <trabajossenacristiana@gmail.com>
2025-10-22 13:01:06 -0500

    Delete AeroSoft directory

--- AeroSoft/backend/controllers/AsientosController.php
+++
@@ -1,9 +0,0 @@
-<?php
-// backend/controladores/AsientosController.php
-require_once __DIR__ . "/../modelos/asientosModel.php";
-
-if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['id_avion'])) {
-    $asientos = AsientosModel::obtenerAsientosDisponiblesPorAvion($_GET['id_avion']);
-    echo json_encode($asientos);
-}
-?>
